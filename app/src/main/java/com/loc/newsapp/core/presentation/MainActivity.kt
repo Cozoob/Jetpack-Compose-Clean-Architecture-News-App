78db07a70c14541238264136cf5e37be0b5d3580
Cozoob <kozubmarcin10@gmail.com>
2024-10-24 23:06:21 +0200

    Add transparent system bars

--- app/src/main/java/com/loc/newsapp/core/presentation/MainActivity.kt
+++ app/src/main/java/com/loc/newsapp/core/presentation/MainActivity.kt
@@ -3,9 +3,13 @@ package com.loc.newsapp.core.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.loc.newsapp.ui.theme.NewsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,6 +24,16 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         setContent {
             NewsAppTheme {
+                val isSystemInDarkMode = isSystemInDarkTheme()
+                val systemUiController = rememberSystemUiController()
+
+                SideEffect {
+                    systemUiController.setSystemBarsColor(
+                        color = Color.Transparent,
+                        darkIcons = !isSystemInDarkMode
+                    )
+                }
+
                 Surface {
                     InitialLoadingScreenRoot()
                 }
